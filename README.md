edb430ef1e131f97d4fe4b2c4eacafe94be5d647
kpeluso <pelusoken@gmail.com>
2018-09-11 01:11:59 -0400

    CV and Lava updates

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ https://devcenter.heroku.com/categories/command-line
 
 https://github.com/nicolewhite/slack-meme/issues/7
 
+## Update Workflow (for my own reference)
+
+$ heroku login
+$ heroku git:clone -a kennyp
+$ cd kennyp;git remote add kennyp https://github.com/kpeluso/kennyp.git ;git pull kennyp master
+$ Make edits
+$ git add .;git commit -m 'update';git push -u origin master;git push heroku master
+
--- src/assets/info/proj.js
+++ src/assets/info/proj.js
@@ -5,9 +5,18 @@ const proj = [
     key: 'proj-1',
     items: [
       {
-        head: 'Head-Controlled RC Car, 2016',
+        head: 'Lava, 2018',
         leaf: true,
         key: 'proj-1-1',
+        items: [
+          'Open-source, decentralized random number generation',
+          <a href="https://github.com/kpeluso/lava" target="_blank">Check it out on GitHub!</a>
+        ]
+      },
+      {
+        head: 'Head-Controlled RC Car, 2016',
+        leaf: true,
+        key: 'proj-1-2',
         items: [
           'Completed in a team of 3',
           'Developed a means of controlling an RC using only head movements for the purpose of prototyping an assistive technology',
@@ -17,7 +26,7 @@ const proj = [
       {
         head: 'Biotech Stock Prediction, 2016',
         leaf: true,
-        key: 'proj-1-2',
+        key: 'proj-1-3',
         items: [
           'Completed in a team of 4',
           'Developed decision tree, logistic regression, SVM, and naïve bayes classifiers in Python to predict the movement of biotech stock prices using clinical trial data, and visualized insights in D3.js'
@@ -26,7 +35,7 @@ const proj = [
       {
         head: 'Wayvepool, 2015',
         leaf: true,
-        key: 'proj-1-3',
+        key: 'proj-1-4',
         items: [
           'Completed during Hack@Brown, 2015',
           'Completed in a team of 4',
