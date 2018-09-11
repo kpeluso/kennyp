d5a8686e98f6ec54064be6a669abf3a78fd136a5
kpeluso <pelusoken@gmail.com>
2018-09-11 01:46:07 -0400

    added mmua project

--- src/assets/info/proj.js
+++ src/assets/info/proj.js
@@ -16,9 +16,18 @@ const proj = [
         ]
       },
       {
-        head: 'Head-Controlled RC Car, 2016',
+        head: 'Meetr, 2018',
         leaf: true,
         key: 'proj-1-2',
+        items: [
+          'A tool for enabling productive and useful meetings.',
+          <a href="https://mmua.herokuapp.com" target="_blank">Try Meetr!</a>
+        ]
+      },
+      {
+        head: 'Head-Controlled RC Car, 2016',
+        leaf: true,
+        key: 'proj-1-3',
         items: [
           'Completed in a team of 3',
           'Developed a means of controlling an RC using only head movements for the purpose of prototyping an assistive technology',
@@ -28,7 +37,7 @@ const proj = [
       {
         head: 'Biotech Stock Prediction, 2016',
         leaf: true,
-        key: 'proj-1-3',
+        key: 'proj-1-4',
         items: [
           'Completed in a team of 4',
           'Developed decision tree, logistic regression, SVM, and naïve bayes classifiers in Python to predict the movement of biotech stock prices using clinical trial data, and visualized insights in D3.js'
@@ -37,7 +46,7 @@ const proj = [
       {
         head: 'Wayvepool, 2015',
         leaf: true,
-        key: 'proj-1-4',
+        key: 'proj-1-5',
         items: [
           'Completed during Hack@Brown, 2015',
           'Completed in a team of 4',
