fff213dd7310b63863598f24164d01f3c652d47f
Kenny P <17100641+kpeluso@users.noreply.github.com>
2021-05-15 01:51:25 -0400

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,13 +10,8 @@ const Landing = () => {
       <Jumbotron className='landing-Comp__tron'>
         <h1 className="display-3">Hello, World!</h1>
         <h1 className="display-3">I'm Kenny Peluso.</h1>
-        <p className="lead">Blockchain and Machine Learning Technologist / Full-Stack Developer @ John Hancock</p>
-        <p className="lead">Long Island, NY <span role="img" aria-label="Airplane">✈️</span> Boston, MA</p>
-        <hr className="my-2" />
-        <p>I'm interested in Data Science, Operations Research, and Entre/Intrapreneurship.</p>
-        <p>I’m always seeking to learn and build.</p>
-        <p>I value the balance between <a href='https://en.wikipedia.org/wiki/Axiomatic_system#Properties' rel="noopener noreferrer" target="_blank">consistency under a set of axioms</a> and simplicity.</p>
-        <p>You can usually find me reading psychology papers, philosophizing, modeling with Python, or developing websites with Meteor and React.</p>
+        <p className="lead">Co-Founder / R&D @ <a href='upshot.io' rel="noopener noreferrer" target="_blank">upshot.io</a></p>
+        <p className="lead">Long Island <> NYC</p>
         <hr className="my-2" />
         <p>Click any button below to learn more about me.</p>
       </Jumbotron>
